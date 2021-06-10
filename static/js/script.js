3669e4b72e51e54f16fa52cde3d9b5b1cfca164b
szilG <gombiszilvi@gmail.com>
2021-06-10 20:23:01 +0000

    create add_recipe  template

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('select').formSelect();
+    $('input#input_text, textarea#textarea2').characterCounter();
+    $('.tooltipped').tooltip();
   });

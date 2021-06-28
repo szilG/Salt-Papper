b72086ceb2856b6c4de8676f217d42df0e442028
szilG <gombiszilvi@gmail.com>
2021-06-28 22:05:30 +0000

    fix add recipe form change for bootstrap components

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,7 @@
 
 
 
-//Swiper - Credit to Vladimir Kharlampidi https://github.com/nolimits4web/Swiper/blob/master/demos/150-freemode.html
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-    centeredSlides: true,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      540: {
-        slidesPerView: 1,
-      },  
-      541: {
-        slidesPerView: 2,  
-      },
-      1024: {
-        slidesPerView: 3,  
-      },
-      1200: {
-        slidesPerView: 4,
-      }
-    }
-  });
+// https://getbootstrap.com/docs/4.4/components/tooltips/
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})

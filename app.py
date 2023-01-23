52d318a6fdd139bb8b0c6f28939565b51bce7b31
Szilvia Gombarovics <64918422+szilG@users.noreply.github.com>
2023-01-23 14:43:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -387,4 +387,4 @@ def single_recipe(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
